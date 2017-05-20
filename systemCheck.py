d37d678ef936ca7c5b18d7aebaee06ca7b75535f
Alex Barbour <JAlexBarbour>
2017-05-19 22:42:25 -0400

    Committing changes to systemCheck.py

--- systemCheck.py
+++ systemCheck.py
@@ -1,25 +1,36 @@
 import platform
 import os
 
-OSstring = "";
-fsDir = "";
+def main():
+	fsDir = checkOS();
 
+	# print fsDir;	
+	if(len(fsDir) > 0): statDirs(fsDir);
+	
+
+# Checks the OS and sets the fsDir variable
 def checkOS():
 	OSstring = platform.platform()
 
-	if "centos-7" in OSstring:
-        	fsDir = "/opt/certdeploy"
-		print OSstring;
-		print fsDir;
-        	return "centos-7";
+        # Checks for CentOS
+	if "centos" in OSstring: print "centos found!"; return  "/opt/certdeploy";
 
-def checkDir():
+# Checks for necessary directories and creates them if needed
+def statDirs(fsDir):
 	if(os.path.exists(fsDir)):
 		print fsDir+" does exist";
 		if(os.path.isdir(fsDir)):
 			print fsDir+" is a directory";
 		else: print fsDir+" is not a directory";
-	else: print fsDir+" does not exist";		
+	else: 
+		print '{} does not exist, creating it now'.format(fsDir);
+		makeDirs(fsDir);		
 
-checkOS();
-checkDir();
+def makeDirs(targetDir):
+	try:
+		os.makedirs(fsDir);
+	except OSError:
+		if not os.path.isdir(fsDir):
+			raise;
+
+main();
